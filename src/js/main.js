846565e0d2bbbde6de8b2d4e9e33bfcf886abb1d
razumeyka <33911265+razumeyka@users.noreply.github.com>
2019-02-17 17:59:01 +0200

    almost ready

--- src/js/main.js
+++ src/js/main.js
@@ -34,9 +34,10 @@ $(document).ready(function(){
 	setInterval(function(){topslider_change()},3000);
 	
 // licenses
-    $('.carousel .next,.carousel .prev').click(function(){
+	
+    $('.license_screen .next, .license_screen .prev').click(function(){
         console.log('click carousel');
-        var countslides=jQuery('.review_container').children().children().length-Math.round(parseFloat(jQuery('.review_wrapper').css('width'))/parseFloat(jQuery('.one_doc').css('width')));
+        var countslides=jQuery('.carousel__container').children().children().length-Math.round(parseFloat(jQuery('.carousel__wrapper').css('width'))/parseFloat(jQuery('.one_doc').css('width')));
         var slide=$(this).closest('.carousel').data('slide');
         console.log(countslides);
         if($(this).hasClass('next')){
@@ -50,6 +51,10 @@ $(document).ready(function(){
          $(this).closest('.carousel').find('.long').css('margin-left','-'+(slide*25)+'%');
 		$(this).closest('.carousel').data('slide',slide);
     });
+	
+// mask
+	
+    $('input[type="tel"]').mask("8-999-999-99-99");
 	
 	
 
